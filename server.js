f29bdc1135350f25ad3459ad6476de8b3f0068b3
Ted Chang <teddadore@gmail.com>
2017-06-23 17:51:36 -0400

    Added Callback

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ var app = express();
 app.locals.baseUrl = process.env.HOSTNAME + ':'+ process.env.PORT;
 console.log(app.locals);
 
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000, function(){
+	console.log("THIS IS MY CALLBACK");
+});
 // var server = app.listen(process.env.PORT);
 // var path = require('path');
 app.use(express.static('public'));
